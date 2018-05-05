60414e792a21afed7d7c24b98cb54d92e4e3e896
Mais Muhtaseb <mais.muhtaseb@gmail.com>
2018-05-05 18:00:21 +0300

    changes on the route

--- react-client/src/components/AppRoute.jsx
+++ react-client/src/components/AppRoute.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Link, IndexRoute, hashHistory, browserHistory , Switch  } from "react-router-dom";
 import $ from 'jquery';
 
-import Home from './Home.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 
@@ -10,7 +9,7 @@ import Signup from './Signup.jsx';
 const AppRoute = (props) => (
   <BrowserRouter history={hashHistory}>
   <Switch>
-  <Route exact path="/" component={Home} />
+  <Route exact path="/" component={Login} />
   <Route exact path="/login" component={Login} />
   <Route exact path="/signup" component={Signup} />
 
