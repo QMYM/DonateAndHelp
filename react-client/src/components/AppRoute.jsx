0f8e6f4228202f0d30b7756dab170de7b25d992e
Mais Muhtaseb <mais.muhtaseb@gmail.com>
2018-05-06 19:39:01 +0300

    router at React

--- react-client/src/components/AppRoute.jsx
+++ react-client/src/components/AppRoute.jsx
@@ -1,6 +1,12 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Link, IndexRoute, hashHistory, browserHistory , Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 import $ from 'jquery';
 
 import Login from './Login.jsx';
@@ -8,14 +14,64 @@ import Signup from './Signup.jsx';
 import Home from './Home.jsx'
 
 
-const AppRoute = (props) => (
-  <BrowserRouter history={hashHistory}>
-  <Switch>
-  <Route exact path="/" component={Login} />
-  <Route exact path="/login" component={Login} />
-  <Route exact path="/signup" component={Signup} />
-  </Switch>
-  </BrowserRouter>
-)
+// const AppRoute = (props) => (
+//   <BrowserRouter history={hashHistory}>
+//   <Switch>
+//   <Route exact path="/" component={Login} />
+//   <Route exact path="/login" component={Login} />
+//   <Route exact path="/signup" component={Signup} />
+//   </Switch>
+//   </BrowserRouter>
+// )
 
-export default AppRoute
+
+// const AuthExample = () => (
+//   <Router>
+//     <div>
+      
+//       <ul>
+//         <li>
+//           <Link to="/public">Public Page</Link>
+//         </li>
+//         <li>
+//           <Link to="/protected">Protected Page</Link>
+//         </li>
+//       </ul>
+//       <Route path="/public" component={Public} />
+//       <Route path="/protected" component={Protected} />
+//     </div>
+//   </Router>
+// );
+
+
+class AppRoute extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+    <div>
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/login">Login Page</Link>
+        </li>
+        <li>
+          <Link to="/register">Sign Up Page</Link>
+        </li>
+      </ul>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Signup} />
+    </div>
+  </Router>
+   </div>)
+  }
+}
+
+
+export default AppRoute;
