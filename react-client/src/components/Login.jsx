4450b729dc50c73c4a5075362031939634418f34
Mohammedalduraidi <Mohd.alduraidi@gmail.com>
2018-05-06 10:31:08 +0300

    sorting the server side

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -6,55 +6,77 @@ import axios from 'axios';
 import Signup from './Signup.jsx'
 
 
-class Login extends React.Component{
+class Login extends React.Component {
 
 
-	constructor(props){
-		super(props);
-		this.state = {
-			userName: '',
-			password: ''
-		}
-	this.handlechangeUserName = this.handlechangeUserName.bind(this);
-		this.handlechangePassword = this.handlechangePassword.bind(this);
-		this.submitLogin = this.submitLogin.bind(this);
-	}
+constructor(props) {
+    super(props);
+    this.state = {
+        userName: '',
+        password: ''
+    }
+    this.handlechangeUserName = this.handlechangeUserName.bind(this);
+    this.handlechangePassword = this.handlechangePassword.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+}
 
-	handlechangeUserName(evt){
-		var user = evt.target.value;
-		this.setState({userName:user});
-	}
+handlechangeUserName(evt) {
+    var user = evt.target.value;
+    this.setState({
+        userName: user
+    });
+}
 
-	handlechangePassword(evt){
-		var pass = evt.target.value;
-		this.setState({password:pass});
-	}
+handlechangePassword(evt) {
+    var pass = evt.target.value;
+    this.setState({
+        password: pass
+    });
+}
 
-	submitLogin(){
-		axios.post("/login", {userName:this.state.userName, password:this.state.password})
-		.then(response => {
-			window.location.href= '/profile';
-		}).catch(error => {
-			console.log('error login',error)
-		})
-	}
+submitLogin() {
+    axios.post("/login", {
+            userName: this.state.userName,
+            password: this.state.password
+        })
+        .then(response => {
+            window.location.href = '/profile';
+        }).catch(error => {
+            console.log('error login', error)
+        })
+}
 
-	render(){
+render() {
 
-		return(
+    return (
 
-			<div>
-			<input type="text" onChange={this.handlechangeUserName}/>
-			<br/>
-			<input type="password" onChange={this.handlechangePassword}/>
-			<br/>
-			<button onClick={this.submitLogin}>Login</button>
-			</div>
+        <
+        div >
+        <
+        input type = "text"
+        onChange = {
+            this.handlechangeUserName
+        }
+        /> <
+        br / >
+        <
+        input type = "password"
+        onChange = {
+            this.handlechangePassword
+        }
+        /> <
+        br / >
+        <
+        button onClick = {
+            this.submitLogin
+        } > Login < /button> <
+        /div>
 
 
 
-			)}
+    )
+}
 
 
-	}
-	export default Login;
+}
+export default Login;
--- server/handler.js
+++ server/handler.js
@@ -2,36 +2,37 @@ let db = require('../db/index');
 let helper = require('../helper/uitilty');
 let bcrypt = require('bcrypt');
 
-exports.Signup=function(req,res){
-	db.userSave(req.body , function(err , data){
-		if(err){res.status(404).send(err)}
-			res.status(200).send(data)
-	})
-}
-
-exports.Login = function(req,res){
- var username = req.body.userName;
- var password = req.body.password;
- db.Users.findOne({username:username}, function(err,data){
- console.log('mais is here',data)
-  if(err){
-    throw err;
-  }
-  else {
-    if (!data) {
-     res.sendStatus(404);
-   }
-   else {
-    bcrypt.compare(password, data.password, function(err,found){
-      if(found) {
-        helper.createSession(req,res,data.username);  
-      }
-      else {
-        res.sendStatus(404);
-      }
+exports.Signup = function(req, res) {
+    db.userSave(req.body, function(err, data) {
+        if (err) {
+            res.status(404).send(err)
+        }
+        res.status(200).send(data)
     })
-  }
-
 }
-})
+
+exports.Login = function(req, res) {
+    var username = req.body.userName;
+    var password = req.body.password;
+    db.Users.findOne({
+        username: username
+    }, function(err, data) {
+        console.log('mais is here', data)
+        if (err) {
+            throw err;
+        } else {
+            if (!data) {
+                res.sendStatus(404);
+            } else {
+                bcrypt.compare(password, data.password, function(err, found) {
+                    if (found) {
+                        helper.createSession(req, res, data.username);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+            }
+
+        }
+    })
 }
