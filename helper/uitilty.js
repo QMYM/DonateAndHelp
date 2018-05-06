e6a52f84e73ee4481fb39befefdfc5899ee50e78
Mohammedalduraidi <Mohd.alduraidi@gmail.com>
2018-05-06 13:47:51 +0300

    fixing some bugs for the signup and login

--- helper/uitilty.js
+++ helper/uitilty.js
@@ -2,14 +2,14 @@ exports.createSession = function (req, res, newUser) {
   req.session.regenerate(function () {
     req.session.user = newUser
     // console.log("hello here",newUser)
-    res.sendStatus(201)
+    res.sendStatus(201)  //after create session for him we let him loggin from here by sending sendStatus 201 to the success function in the front end.
   })
 }
 
 // check if the user is logged in
 exports.isLoggedIn = function (req, res) {
   if (req.session) {
-    return !!req.session.user
+    return !!req.session.user 
   }
   return false
 }
