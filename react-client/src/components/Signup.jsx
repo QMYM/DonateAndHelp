3565860e809cbafbaa80dc5ad97641f8e48f8488
QaysTrad <qaystrad@gamil.com>
2018-05-06 11:27:17 +0300

    mocha test database

--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -1,79 +1,64 @@
-import React from 'react';
-import $ from 'jquery';
-
+import React from 'react'
+import $ from 'jquery'
 
 class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            email: "",
-            password: ""
-        };
-        this.onChange = this.onChange.bind(this);
-        this.submit = this.submit.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
     }
-    onChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+    this.onChange = this.onChange.bind(this)
+    this.submit = this.submit.bind(this)
+  }
+  onChange (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+  submit (username, email, password) {
+    $.ajax({
+      type: 'POST',
+      url: '/user',
+      data: {
+        username: username,
+        email: email,
+        password: password
 
-    };
-    submit(username, email, password) {
-        $.ajax({
-            type: 'POST',
-            url: '/user',
-            data: {
-                username: username,
-                email: email,
-                password: password,
+      },
+      success: (data) => {
+        console.log('aaa', data)
+      }
+    })
+  };
 
-            },
-            success: (data) => {
-                console.log('aaa', data)
-            }
-        });
-    };
+  render () {
+    return (<div >
+      <h2 > Name: </h2> <input type='text'
+        name='username'
+        onChange={
+          this.onChange
+        }
+      /> <h2 > Email: </h2>
 
+      <input type='text'
+        name='email'
+        onChange={
+          this.onChange
+        }
+      /> <h2 > Password: </h2> <input type='password'
+        name='password'
+        onChange={
+          this.onChange
+        }
+      />
 
-
-
-    render() {
-        return ( <
-            div >
-            <
-            h2 > Name: < /h2> <
-            input type = 'text'
-            name = 'username'
-            onChange = {
-                this.onChange
-            }
-            /> <
-            h2 > Email: < /h2>
-
-            <
-            input type = 'text'
-            name = 'email'
-            onChange = {
-                this.onChange
-            }
-            /> <
-            h2 > Password: < /h2> <
-            input type = 'password'
-            name = 'password'
-            onChange = {
-                this.onChange
-            }
-            />
-
-            <
-            button onClick = {
-                () => this.submit(this.state.username, this.state.email, this.state.password)
-            } > Signup < /button> <
-            /div>
-        )
-    }
-
+      <button onClick={
+        () => this.submit(this.state.username, this.state.email, this.state.password)
+      } > Signup </button> </div>
+    )
+  }
 };
 
-export default Signup;
+export default Signup
--- server/index.js
+++ server/index.js
@@ -1,32 +1,27 @@
-let express = require('express');
+let express = require('express')
 let db = require('../db/index')
-let helper = require('../helper/uitilty');
-let session = require('express-session');
-let bcrypt = require ('bcrypt');
-let bodyParser = require('body-parser');
-let handler = require('./handler');
+let helper = require('../helper/uitilty')
+let session = require('express-session')
+let bcrypt = require('bcrypt')
+let bodyParser = require('body-parser')
+let handler = require('./handler')
 
-const app = express();
-const saltRounds = 10;
+const app = express()
+const saltRounds = 10
 
-app.use(express.static(__dirname + '/../react-client/dist'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static(__dirname + '/../react-client/dist'))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(session({
- secret: "very very secret",
- resave: true,
- saveUninitialized: true
+  secret: 'very very secret',
+  resave: true,
+  saveUninitialized: true
 }))
 
+app.post('/login', handler.Login)
+app.post('/user', handler.Signup)
 
-
-app.post('/login', handler.Login);
-app.post('/user',handler.Signup);
-
-
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`The Port : ${ PORT }`);
-});
+  console.log(`The Port : ${PORT}`)
+})
