90bf4586172d5844dc34157249a6243c9f04f57d
QaysTrad <qaystrad@gamil.com>
2018-05-07 16:23:34 +0300

    benf page

---
+++ react-client/src/components/Beneficiaries.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import $ from 'jquery'
+import axios from 'axios'
+
+class Beneficiaries extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+
+		}
+	}
+	render(){
+		return (
+			<div>
+			
+			</div>
+			)
+	}
+}
