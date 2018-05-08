48f39bc8a26c9698bd2948e62251bd31469bf11e
yussur90 <yuyualani@yahoo.com>
2018-05-08 10:18:26 +0300

    .

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-=======
-import React from 'react'
-import ReactDOM from 'react-dom'
 import AppRoute from './components/AppRoute.jsx'
->>>>>>> 0bb383269cb2429601e59adcecb8016a61999dee
+
 
 class App extends React.Component {
   constructor (props) {
@@ -20,13 +17,9 @@ class App extends React.Component {
 
   render () {
     return (<div>
-<<<<<<< HEAD
-   
-    <Login/>
-    <Signup/>
-=======
+
       <AppRoute />
->>>>>>> 0bb383269cb2429601e59adcecb8016a61999dee
+
     </div>)
   }
 }
