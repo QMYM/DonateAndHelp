df839ac19e4f0fb3e3df7d7389c0457952946e05
Mohammedalduraidi <Mohd.alduraidi@gmail.com>
2018-05-10 23:29:22 +0300

    testing the response status of post and get request

--- test/test.js
+++ test/test.js
@@ -1,9 +1,260 @@
-var assert = require('chai').assert
-var handler = require('../server/handler')
-var db = require('../db/index')
+ var assert = require('chai').assert
+ var handler = require('../server/handler')
+ var server = require('../server/index')
+ var db = require('../db/index')
+var chai = require('chai');  
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+var should = chai.should();
+  var chai = require('chai')
+  , chaiHttp = require('chai-http');
+ 
+ chai.use(chaiHttp);
+
+describe('/searchBeneficiary', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/searchBeneficiary')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
 
 
 
+describe('/uploadImageDonor', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/uploadImageDonor')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+describe('/addProfileDonor', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/addProfileDonor')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+
+
+describe('/postCampaign', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/postCampaign')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+
+describe('/uploadImageCampaign', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/uploadImageCampaign')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+
+
+describe('/addProfileCompany', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/addProfileCompany')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+describe('/uploadImage', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/uploadImage')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+
+
+describe('/SignupCompany', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/SignupCompany')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+ describe('/Signup', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/Signup')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+ describe('/LoginCompany', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/LoginCompany')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+
+  describe('/LoginDonater', function () {
+      it('it should sendStatus(404)if there is somthing wrong okay!', function(done){
+        chai.request(server)
+           .post('/LoginDonater')
+            .end(function(err,res) {
+                res.should.have.status(404);
+               
+              done();
+            });
+      });
+  });
+  describe('/sessionName', function () {
+      it('it should GET all the sessionNames', function(done){
+        chai.request(server)
+           .get('/sessionName')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+
+   describe('/getImage', function () {
+      it('it should GET all the getImage', function(done){
+        chai.request(server)
+           .get('/getImage')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+describe('/logout', function () {
+      it('it should GET all the logout', function(done){
+        chai.request(server)
+           .get('/logout')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+
+describe('/reciveMessag', function () {
+      it('it should GET all the reciveMessag', function(done){
+        chai.request(server)
+           .get('/reciveMessag')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+
+describe('/getImageDonor', function () {
+      it('it should GET all the getImageDonor', function(done){
+        chai.request(server)
+           .get('/getImageDonor')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+
+describe('/fetchDonorData', function () {
+      it('it should GET all the fetchDonorData', function(done){
+        chai.request(server)
+           .get('/fetchDonorData')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+describe('/fetchCompanyData', function () {
+      it('it should GET all the fetchCompanyData', function(done){
+        chai.request(server)
+           .get('/fetchCompanyData')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+
+describe('/imageSearch', function () {
+      it('it should GET all the imageSearch', function(done){
+        chai.request(server)
+           .get('/imageSearch')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
+describe('/donorCam', function () {
+      it('it should GET all the donorCam', function(done){
+        chai.request(server)
+           .get('/donorCam')
+            .end(function(err,res) {
+                res.should.have.status(200);
+               
+              done();
+            });
+      });
+  });
 
 describe('userCompany', function () {
   describe('#save()', function () {
@@ -104,4 +355,8 @@ describe('companyCampaigns', function () {
     })
   })
 })
+
+
+
+
 
