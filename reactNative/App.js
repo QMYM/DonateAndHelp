b85ffdb3fc50f1afaae7b912d75abdfb65dbc438
QaysTrad <qaystrad@gamil.com>
2018-05-16 15:54:52 +0300

    signup react naitve first page

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,33 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View , TextInput  , Button , Alert} from 'react-native';
+import { StyleSheet, Text, View , TextInput  , Button , FlatList, ActivityIndicator,  Alert} from 'react-native';
+import axios from 'axios'
+import promise from 'promise'
 
 export default class App extends React.Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      text: ''
-    }
-  }
-   _onPressButton(text) {
-    Alert.alert("Hi " , text)
-  }
+     username: '',
+     password:'',
+     email:''
+   }
+ }
+//  _onPressButton(username) {
+//   // Alert.alert("Hi " , text)
+//   console.log("ddddaa")
+//     axios.get('http://192.168.1.105:3000/donorCam')
+//     .then(function (res) {
+//       console.log("hello2" , res)
+//     })
+//   // this.user()
+// }
+  send (username,password,email) { // sending post reqeust to the server
+    console.log("good Click" , username)
+    axios.post('http://192.168.1.105:3000/Donater',
+     {
+      username: username,
+      password:password,
+      email:email
+    })
+    .then(function (res) {
+        console.log('hello3' , res)
+    }).catch(function (err) {
+      console.log("err" , err)
+    })
+}
+
+// user(){
+//   console.log("aaa")
+//     axios.get('https://192.168.1.105/donorCam')
+//     .then(function (res) {
+//       console.log("hello2" , res)
+//     })
+//   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome To our App</Text>
-        <Text>Enter Anything</Text>
-         <TextInput
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text>So Cool Right !! </Text>
-            <Button
-            onPress={() =>this._onPressButton(this.state.text)}
-            title="Press Me"
-          />
+      <Text>Welcome To our App</Text>
+      <Text>User Name : </Text>
+      <TextInput
+      placeholder="Type here your username!"
+      onChangeText={(username) => this.setState({username})}
+      />
+      <Text>Email : </Text>
+      
+      <TextInput
+      placeholder="Type here your email!"
+      onChangeText={(email) => this.setState({email})}
+      />
+      <Text>Password : </Text>
+
+      <TextInput secureTextEntry={true}
+      placeholder="Type here your password"
+      onChangeText={(password) => this.setState({password})}
+      />
+      <Text>So Cool Right !! </Text>
+  
+        <Button
+      onPress={() => this.send(this.state.username,this.state.password,this.state.email)}
+      title="Post Me"
+      />
       </View>
-    );
+      );
   }
 }
 
