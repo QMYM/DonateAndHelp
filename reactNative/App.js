ecbd86adf27292bc1f8f40e68c48e4030a266af6
yussur90 <yuyualani@yahoo.com>
2018-05-17 17:45:25 +0300

    routing and login page

--- reactNative/App.js
+++ reactNative/App.js
@@ -6,72 +6,54 @@ import axios from 'axios'
 import {
   Scene,
   Router,
-  Actions,
-  Reducer,
-  ActionConst,
-  Overlay,
-  Tabs,
-  Modal,
-  Drawer,
-  Stack,
-  Lightbox,
+  Actions
+
 } from 'react-native-router-flux';
+
+import Signup from "./components/Signup"
+import Login from "./components/Login"
+import Home from "./components/Home"
+import Donor from "./components/Donor"
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     username: '',
-     password:'',
-     email:''
+   
    }
  }
 
-  send (username,password,email) { // sending post reqeust to the server
-    console.log("good Click" , username)
-    axios.post('http://192.168.1.105:3000/Donater',
-     {
-      username: username,
-      password:password,
-      email:email
-    })
-    .then(function (res) {
-        console.log('hello3' , res)
-    }).catch(function (err) {
-      console.log("err" , err)
-    })
-}
 
 
   render() {
     return (
-    <View style={styles.container}>
-      <Text>Welcome To our App</Text>
-      <Text>User Name : </Text>
-      <TextInput
-      placeholder="Type here your username!"
-      onChangeText={(username) => this.setState({username})}
-      />
-      <Text>Email : </Text>
+    <Router>
+      <Scene key="root">
+        <Scene key="Home"
+          component={Home}
+          title="Home"
+          initial
+        />
+        <Scene
+          key="Login"
+          component={Login}
+          title="Gray"
+        />
       
-      <TextInput
-      placeholder="Type here your email!"
-      onChangeText={(email) => this.setState({email})}
-      />
-      <Text>Password : </Text>
-
-      <TextInput secureTextEntry={true}
-      placeholder="Type here your password"
-      onChangeText={(password) => this.setState({password})}
-      />
-      <Text>So Cool Right !! </Text>
-  
-        <Button
-      onPress={() => this.send(this.state.username,this.state.password,this.state.email)}
-      title="Post Me"
-      />
-      </View>
+          <Scene key="Signup"
+          component={Signup}
+          title="Signup"
+          
+        />
+          <Scene key="Donor"
+           hideNavBar={true}
+          component={Donor}
+          title="Donor"
+          
+        />
+      </Scene>
+    </Router>
       );
   }
 }
