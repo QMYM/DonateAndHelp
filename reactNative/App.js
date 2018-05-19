a9db0b8bba6e6c9e5f8dbb0ac4f2e8f05d4d5500
yussur90 <yuyualani@yahoo.com>
2018-05-19 21:55:43 +0300

    tab navigation bar

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry,StyleSheet, Text, View , TextInput  , Button , FlatList, ActivityIndicator,  Alert} from 'react-native';
 import axios from 'axios'
-
+import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json
 
 import {
   Scene,
@@ -10,12 +10,7 @@ import {
 
 } from 'react-native-router-flux';
 
-import Signup from "./components/Signup"
-import Login from "./components/Login"
-import Home from "./components/Home"
-import Donor from "./components/Donor"
-import Donor_Campaign from "./components/Donor_Campaign"
-import Message from "./components/Message"
+import AppRoute from "./components/AppRoute"
 
 
 export default class App extends React.Component {
@@ -25,55 +20,11 @@ export default class App extends React.Component {
    
    }
  }
-
   render() {
     return (
-    <Router>
-      <Scene key="root">
-        <Scene key="Home"
-          component={Home}
-          title="Home"
-          initial
-
-        />
-        <Scene
-          key="Login"
-          component={Login}
-          title="Login"
-        />
-      
-          <Scene key="Signup"
-          component={Signup}
-          title="Signup"
-          
-        />
-          <Scene key="Donor"
-           hideNavBar={true}
-          component={Donor}
-          title="Donor"
-          
-        />
-        <Scene key="Donor_Campaign"
-          component={Donor_Campaign}
-          title="Donor_Campaign"
-          
-        />
-        <Scene key="Message"
-          component={Message}
-          title="Message"
-          
-        />
-      </Scene>
-    </Router>
+      <AppRoute/>
       );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- reactNative/components/Home.js
+++ reactNative/components/Home.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View , TextInput   , FlatList, ActivityIndicator,  Alert} from 'react-native';
 import axios from 'axios'
 import { Actions } from 'react-native-router-flux'; 
+import { TabNavigator } from 'react-navigation'; // Version can be specified in package.json
 import { Button } from 'react-native-elements';
 
+import Login from './Login'
+import Signup from './Signup'
+
+const Yussur =  TabNavigator({
+  Home: { screen: Login },
+  Settings: { screen: Signup },
+});
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -14,17 +22,8 @@ class Home extends React.Component {
  }
  render() {
     return (
-      <View style={styles.container}>
-      <Text>
-      welcome Home
-      </Text>
-      <Button onPress={() => Actions.Signup()}
-            title="Signup" />
-            
-            <Button onPress={() => Actions.Login()}
-            title="Login" />
+        <Yussur/>
 
-      </View>
       );
   }
 }
@@ -39,5 +38,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
 
 module.exports = Home;
