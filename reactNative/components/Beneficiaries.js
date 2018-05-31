fdc535ece33700def8d813e7280218eaca384e52
yussur90 <yuyualani@yahoo.com>
2018-05-31 18:58:27 +0300

    style on donor and befeshries

--- reactNative/components/Beneficiaries.js
+++ reactNative/components/Beneficiaries.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Modal, TouchableHighlight, StyleSheet, Text, View, TextInput, Button, FlatList, ActivityIndicator, Alert,Image} from 'react-native'
+import { Modal, TouchableHighlight, StyleSheet, Text, View, TextInput, FlatList, ActivityIndicator, Alert,Image} from 'react-native'
 import axios from 'axios'
 import { Actions } from 'react-native-router-flux'
-import { Container, Content} from 'native-base'
+import { Button } from 'react-native-elements'
+import { Container, Header, Item, Input, Icon,  Content} from 'native-base'
 
 function searching (term) {
   return function (x) {
@@ -16,38 +17,64 @@ class Beneficiaries extends React.Component {
     this.state = {
       camp: [],
       amount: '',
-      term: ''
+      term: '',
+      modalVisible: false
 
     }
   }
+
+
 
   componentDidMount () {
     var x = this
     axios.get('https://donatandhelp.herokuapp.com/donorCam')
       .then(function (res) {
-        console.log(res.data,"hi")
         x.setState({camp: res.data})
       }).catch(function (err) {
         console.log(err)
       })
   }
 
+ 
+ 
   render () {
     return (
        <Container>
+       <Header searchBar rounded>
+          <Item>
+            <Icon name='ios-search' />
+            <Input placeholder='Search' onChangeText={(term) => this.setState({term})} />
+            <Icon name='ios-people' />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+
+
         <Content>
+
       <View style={styles.container}>
-        <TextInput placeholder='Search' onChangeText={(term) => this.setState({term})} />
+
+
+     
+
+      
+
         {this.state.camp.filter(searching(this.state.term)).map(item =>
 
           <View style={styles.campview} key={item._id}>
-          <Image source={{uri: item.campaignImage || 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/23473196_915825728566887_4239089197692234922_n.jpg?_nc_cat=0&oh=c8d8c960fe956f1573fa8072743d69f2&oe=5B8FBDE3'}}
+          <Image 
+          source={{uri: item.campaignImage || 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/23473196_915825728566887_4239089197692234922_n.jpg?_nc_cat=0&oh=c8d8c960fe956f1573fa8072743d69f2&oe=5B8FBDE3'}}
               style={styles.img}/>
           
             <Text style={{fontWeight: 'bold', textAlign: 'center'}}>{item.campaignName}</Text>
             <Text>{item.campaignDescription}</Text>
-            <Button
-              title='take' />
+            <Text>{item.campaignAmount}</Text>
+
+         
+
+            
           </View>
         )}
       </View>
@@ -64,23 +91,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:50,
+    marginTop:20,
   },
   campview: {
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    height: 150,
+    height: 300,
     backgroundColor: 'white',
     borderRadius: 10,
     borderWidth: 3,
-    borderColor: '#d6d7da'
+    borderColor: '#d6d7da',
+     alignItems: 'center',
+    justifyContent: 'center',
   },
    img:{
-    width : 40,
-    height:40,
+    width : 60,
+    height:60,
     justifyContent: 'center',
-  }
+  },
+ 
+
 })
 
 module.exports = Beneficiaries
