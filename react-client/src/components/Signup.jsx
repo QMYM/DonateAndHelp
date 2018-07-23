5d70e5789c8707d2ab8836b3050b0876d6844e6e
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-07-23 22:01:59 +0300

    fix the valid password

--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -75,9 +75,7 @@ class Signup extends React.Component {
      alert("Please enter a valid passowrd");
       } else if(password.length < 5) {
          alert("Password length must be at least 5 characters. Please enter a valid password!")
-         } else if(password.length > 8) {
-            alert("Password length must be maximum 8 characters. Please enter a valid password!")
-            } else if(!(/[0-9]/.test(password) && /[a-z]/.test(password) && /[A-Z]/.test(password) && /[!@#$%^&*]/.test(password)) ){
+         } else if(!(/[0-9]/.test(password) && /[a-z]/.test(password) && /[A-Z]/.test(password) && /[!@#$%^&*]/.test(password)) ){
                alert("Password should have at least one number, one upper case, one small case and one speciall character. Please enter a valid one!");
                } else if (confirmPassword === password) {
                   axios.post('/Donater', {username: username,
